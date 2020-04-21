61945782cfe05b6524b7d2c9e142e92c24e10fee
Amine Akhouad <akhouadme@gmail.com>
2020-04-21 21:32:57 +0200

    fix app issue

--- app.js
+++ app.js
@@ -6,14 +6,18 @@ const cookie = require('cookie');
 const nonce = require('nonce')();
 const querystring = require('querystring');
 const request = require('request-promise');
+const path = require('path')
 
 const apiKey = process.env.SHOPIFY_API_KEY;
 const apiSecret = process.env.SHOPIFY_API_SECRET;
 const scopes = 'read_products';
 const forwardingAddress = process.env.BASE_URL
 
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine', 'hbs')
+
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.render('index')
 });
 
 app.get('/shopify', (req, res) => {
@@ -65,36 +69,37 @@ app.get('/shopify/callback', (req, res) => {
         };
         
         if (!hashEquals) {
-            const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
-            const accessTokenPayload = {
-              client_id: apiKey,
-              client_secret: apiSecret,
-              code,
-            };
-            
-            request.post(accessTokenRequestUrl, { json: accessTokenPayload })
-            .then((accessTokenResponse) => {
-              const accessToken = accessTokenResponse.access_token;
-            
-              const shopRequestUrl = 'https://' + shop + '/admin/api/2020-04/shop.json';
-              const shopRequestHeaders = {
-                'X-Shopify-Access-Token': accessToken,
-              };
-
-              request.get(shopRequestUrl, { headers: shopRequestHeaders })
-                .then((shopResponse) => {
-                  res.end(shopResponse);
-                })
-                .catch((error) => {
-                  res.status(error.statusCode).send(error.error.error_description);
-                });
-            })
-            .catch((error) => {
-              res.status(error.statusCode).send(error.error.error_description);
-            });
+          return res.status(400).send('HMAC validation failed');
         }
-        
-        res.status(200).send('HMAC validated');
+    
+        // DONE: Exchange temporary code for a permanent access token
+        const accessTokenRequestUrl = 'https://' + shop + '/admin/oauth/access_token';
+        const accessTokenPayload = {
+          client_id: apiKey,
+          client_secret: apiSecret,
+          code,
+        };
+    
+        request.post(accessTokenRequestUrl, { json: accessTokenPayload })
+        .then((accessTokenResponse) => {
+          const accessToken = accessTokenResponse.access_token;
+          // DONE: Use access token to make API call to 'shop' endpoint
+          const shopRequestUrl = 'https://' + shop + '/admin/api/2020-04/shop.json';
+          const shopRequestHeaders = {
+            'X-Shopify-Access-Token': accessToken,
+          };
+    
+          request.get(shopRequestUrl, { headers: shopRequestHeaders })
+          .then((shopResponse) => {
+            res.status(200).end(shopResponse);
+          })
+          .catch((error) => {
+            res.status(error.statusCode).send(error.error.error_description);
+          });
+        })
+        .catch((error) => {
+          res.status(error.statusCode).send(error.error.error_description);
+        });
     } else {
       res.status(400).send('Required parameters missing');
     }
